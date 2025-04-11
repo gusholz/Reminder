7b238287d8f732a3c5a415a4579b319a0ebd591c
Gustavo Holzmann <gustavobarretohv@live.com>
2025-04-11 14:35:51 -0300

    feat: create `EditListScreen`

---
+++ Reminder/Reminder/Views/Scenes/EditList/EditListScreen.swift
@@ -0,0 +1,30 @@
+//
+//  CreateListView.swift
+//  Reminder
+//
+//  Created by Gustavo Holzmann on 11/04/25.
+//
+
+import SwiftUI
+
+struct EditListScreen: View {
+    var body: some View {
+        VStack(spacing: 16) {
+            TextArea()
+            
+            Divider()
+                .overlay(.cinzaLabels)
+            
+            ColorCarousel()
+            
+            Divider()
+                .overlay(.cinzaLabels)
+            
+            IconCarousel()
+        }
+    }
+}
+
+#Preview {
+    CreateListScreen()
+}
